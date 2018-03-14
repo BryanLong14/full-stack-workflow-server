b902b24ab960f0c9cf97f5e692bd9fd1900bba2b
CJ R <cj@null.computer>
2018-03-14 11:50:19 -0600

    :sparkles: Add all dogs route

---
+++ api/dogs.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const router = express.Router();
+
+const queries = require('../db/queries');
+
+// ALL ROUTES will be prepended by /api/v1/dogs/
+
+router.get('/', async (req, res) => {
+  const dogs = await queries.getAll();
+  res.json(dogs);
+  //
+  // queries.getAll()
+  //   .then(dogs => {
+  //     res.json(dogs);
+  //   });
+});
+
+module.exports = router;
